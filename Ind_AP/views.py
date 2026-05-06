acb7096faa4c89827932bf0030bbd75208a4fd5e
MReddeppa888 <reddyreddeppa888@gmail.com>
2026-05-06 10:20:02 +0530

    commit2

--- Ind_AP/views.py
+++ Ind_AP/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def chittoor(request):
-    return render (request,"chittoor.html")
+    user_name = "Tony"
+    age=24
+    context ={"name" :user_name,"age":age}
+    return render (request,"chittoor.html",context)
 
     # response="<h1> Welcome to Chittoor</h1>"
     # return HttpResponse(response)
--- Ind_Tel/views.py
+++ Ind_Tel/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hyderabad(request):
-    return render (request,"hyderabad.html")
+
+    places = ["Golkonda", "Sanghi", "Charminar", "Chilkur"]
+    context = {"places" : places}
+    return render(request, "hyderabad2.html", context)
+
+    # return render (request,"hyderabad.html")
 
     # response="<h1> Welcome to Hyderadad</h1>"
     # return HttpResponse(response)
